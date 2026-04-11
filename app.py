8cf0958f61b1e7c9270ded3a9adb51ffcc87e8bd
Sidney So Tsin Hui <SIDNEY.SO.TSIN@student.mmu.edu.my>
2026-04-11 21:44:15 +0800

    built /random API and return JSON response

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 import random
 
 app = Flask(__name__)
 
-recipes = ["Nasi Lemak", "Fried Rice", "Pasta", "Burger"]
+# Sample data (can be replaced with database later)
+recipes = ["Fried Rice", "Pancake", "Spaghetti", "Salad"]
 
 @app.route('/')
 def home():
+    return "Omakase Home"
+
+@app.route('/random')
+def random_recipe():
     recipe = random.choice(recipes)
-    return f"<h1>Omakase Today</h1><p>You should eat: {recipe}</p>"
+    return jsonify({"recipe": recipe})
 
 if __name__ == '__main__':
     app.run(debug=True)
