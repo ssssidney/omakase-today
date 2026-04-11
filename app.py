d02a67643082070357fc3c08f480f7547b7901cb
Sidney So Tsin Hui <SIDNEY.SO.TSIN@student.mmu.edu.my>
2026-04-11 20:37:12 +0800

    add random recipe function

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+recipes = ["Nasi Lemak", "Fried Rice", "Pasta", "Burger"]
+
 @app.route('/')
 def home():
-    return "Hello Omakase!"
+    recipe = random.choice(recipes)
+    return f"<h1>Omakase Today</h1><p>You should eat: {recipe}</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
