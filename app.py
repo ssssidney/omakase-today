8f3bc3c8cfd880cc9faf9602bb2be7990c8017c6
Sidney So Tsin Hui <SIDNEY.SO.TSIN@student.mmu.edu.my>
2026-04-12 18:39:45 +0800

    connected frontend with Flask backend using fetch API

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
 
 app = Flask(__name__)
+
+recipes = ["Pancake", "Spaghetti", "Fried Rice", "Chicken Rice"]
 
 @app.route('/')
 def home():
@@ -13,6 +16,12 @@ def about():
 @app.route('/random-page')
 def random_page():
     return render_template('random.html')
+
+@app.route('/random')
+def get_random_recipe():
+    return jsonify({
+        "recipe": random.choice(recipes)
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
